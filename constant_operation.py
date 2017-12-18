63e50ea152a5bd17c2dd607c8b6fb81b1d3e65e7
Shivam Agarwal <shivam.mcs17.du@gmail.com>
2017-12-18 17:45:37 +0530

    Create constant_operation.py

---
+++ constant_operation.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+a= tf.constant(3.,name="input_a")
+b= tf.constant(4.,name="input_b")
+c= tf.multiply(a,b,name="mul_c")
+d= tf.add(a,b,name="add_d")
+e=tf.add(c,d,name="add_e")
+sess= tf.Session()
+output= sess.run(e)
+writer = tf.summary.FileWriter('./my_graph',sess.graph)
+writer.close()
+sess.close()
